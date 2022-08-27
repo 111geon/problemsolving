55c0cc4e6fb96dd3bed01f048f656da7844dadc2
111geon <yangdaz3@gmail.com>
2022-08-27 11:56:56 +0900

    update

--- baekjoon/최소비용구하기.py
+++ baekjoon/최소비용구하기.py
@@ -1,44 +1,43 @@
 import sys
-import collections
 import heapq
 
 input = sys.stdin.readline
 
 def main():
     n = int(input())   
-    graph = collections.defaultdict(list)
+    graph = [[] for _ in range(n+1)]
 
     for _ in range(int(input())):
-        a, b, cost = map(int, input().split())
-        graph[a].append((b, cost))
+        a, b, c = map(int, input().split())
+        graph[a].append((b, c))
 
     a, b = map(int, input().split())
-    dist = [sys.maxsize for _ in range(n+1)]
-    dist[a] = 0
-    visited = set()
+    cost = [sys.maxsize for _ in range(n+1)]
+    cost[a] = 0
+    visited = [False for _ in range(n+1)]
     q = [(0, a)]
 
-    while b not in visited:
-        while q:
-            next_node = heapq.heappop(q)[1]
-            if next_node not in visited: break 
-        visited.add(next_node)
+    while q:
+        curr = heapq.heappop(q)[1]
+        if visited[curr]: continue
+        visited[curr] = True
+        if curr == b: break
+        for nexthop, nextcost in graph[curr]:
+            if cost[curr] + nextcost < cost[nexthop]:
+                cost[nexthop] = cost[curr] + nextcost
+                heapq.heappush(q, (cost[nexthop], nexthop))
 
-        for cand, w in graph[next_node]:
-            dist[cand] = min(dist[cand], dist[next_node] + w)
-            heapq.heappush(q, (dist[cand], cand))
-
-    print(dist[b])
+    print(cost[b])
 
 main()
 
 """
-- 다익스트라 알고리즘은 시작점 노드를 기준으로 다른 노드들과의 거리를 담는 distance 배열이
-  필요하다. 또 이 배열을 갱신할 때마다 cost를 기준으로 정렬한 heap에 추가하여 가장 작은
-  distance를 갖는 노드를 찾아낸다.
+- 다익스트라 알고리즘은 시작점 노드를 기준으로 다른 노드들과의 거리를 담는 cost 배열이 필요하다.
+  또 이 배열을 갱신할 때마다 cost를 기준으로 정렬한 heap에 추가하여 가장 작은 cost를 갖는 노드를
+  찾아낸다.
 - 도착노드를 방문할 때까지 loop을 돌린다.
-- heap에서 노드들을 꺼내며 아직 방문하지 않은 노드 중에 가장 dist가 작은 노드를 찾는다
-- 그 노드를 방문하면서 그 노드와 연결된 다른 노드들을 peek하며 distance를 갱신한다.
-    dist[cand] = min(dist[cand], dist[next_node] + w)
-- 이때, heap에 갱신되는 노드들을 추가해주어야 한다.
+- heap에서 노드들을 꺼내며 가장 비용이 작은 노드를 찾는다
+- 해당 노드는 이미 방문되었지만 예전 cost가 q에 남아있는 경우일 수 있다. 방문된 경우 continue.
+- 그 노드를 방문하면서 그 노드와 연결된 다른 노드들을 peek하며 cost를 갱신할 수 있는 경우
+  갱신을 하고 heap에 갱신한 노드와 비용을 추가한다.
 """
--- baekjoon/특정한최단경로.py
+++ baekjoon/특정한최단경로.py
@@ -6,7 +6,7 @@ def main():
     graph = [[] for _ in range(n+1)]
     for _ in range(e):
         a, b, cost = map(int, input().split())
-        graph[a].append((b, cost))
+        graph[a].append((b, cost))  # (node, cost)
         graph[b].append((a, cost))
     
     u, v = map(int, input().split())
@@ -15,17 +15,35 @@ def main():
     route_un = dijkstra(n, graph, u, n)
     route_vn = dijkstra(n, graph, v, n)
     route_uv = dijkstra(n, graph, u, v)
-    # if (route_uv == sys.maxsize)
-    print(route_uv + min(route_1u + route_vn, route_1v + route_un))
+    if route_uv == -1:
+        print(-1)
+    elif route_1u == -1 or route_vn == -1:
+        print(-1)
+    else:
+        print(route_uv + min(route_1u + route_vn, route_1v + route_un))
 
 def dijkstra(n, graph, a, b):
     cost = [sys.maxsize for _ in range(n+1)]
-    for e in graph[a]: cost[e[0]] = e[1]
-    visited = [False for _ in range(n+1)]
-    visited[a] = True
-    q = []
-    for i in range(1, n+1): heapq.heappush(q, (cost[i], i))
-    print(q)
+    cost[a] = 0
 
+    q = [(0, a)]  # (cost, node)
+    while q:
+        curr = heapq.heappop(q)[1];
+        if curr == b: return cost[b]
+        for nexthop, ncost in graph[curr]:
+            if cost[curr] + ncost <= cost[nexthop]:
+                cost[nexthop] = cost[curr] + ncost
+                heapq.heappush(q, (cost[nexthop], nexthop))
+    
+    return -1
 
 main()
+
+"""
+- 다익스트라 알고리즘
+- 1 -> u -> v -> n 또는 1 -> v -> u -> n 두 경로가 있다. 각각 비용을 구해서 비교하자.
+- 이 그래프의 경우 cost가 0인 경우가 없으므로, 비용 갱신 시 이미 방문한 노드는 갱신될 일이
+  없어 visited 배열 또한 필요가 없다.
+- 출발노드와 도착노드가 단절되어 있는 경우 q가 모두 소진되어도 cost를 반환할 수 없다.
+- uv가 끊어져 있거나, 1 또는 n이 동떨어져있는 경우 전체 경로는 발생할 수 없다.
+"""
