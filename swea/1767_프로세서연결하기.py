e7cc3d815d2c3f282098042ba51f167f8d8f4b0f
Young Geon <54384261+111geon@users.noreply.github.com>
2022-08-29 18:30:36 +0900

    Update 1767_프로세서연결하기.py

--- swea/1767_프로세서연결하기.py
+++ swea/1767_프로세서연결하기.py
@@ -1,142 +1,65 @@
+MAX_INT = 2**63-1
+DX = [-1, 1, 0, 0]
+DY = [0, 0, 1, -1]
+
 def main():
-    for t in range(1, int(input())+1):
+    tc = int(input())
+    for t in range(1, tc+1):
         n = int(input())
         cores = []
-        for _ in range(n):
-            cores.append(list(map(int, input().split())))
+        for _ in range(n): cores.append(list(map(int, input().split())))
 
-        to_connect = []
+        to_conn = []
         for i in range(1, n-1):
             for j in range(1, n-1):
-                if cores[i][j] == 1: to_connect.append((i, j))
+                if cores[i][j] == 1: to_conn.append((i, j))
 
-        max_connection = [0]
-        min_wire = [2**63-1]
-        solution(n, cores, 0, max_connection, to_connect, 0, min_wire)
+        max_conn = [0]
+        min_wire = [MAX_INT]
+        dfs(n, cores, to_conn, 0, max_conn, 0, min_wire)
         print("#" + str(t), min_wire[0])
 
-def solution(n, cores, connection, max_connection, to_connect, wire, min_wire):
-    directions = ["up", "down", "right", "left"]
-
-    if not to_connect:
-        if connection > max_connection[0]:
+def dfs(n, cores, to_conn, conn, max_conn, wire, min_wire):
+    if not to_conn:
+        if conn > max_conn[0]:
             min_wire[0] = wire
-            max_connection[0] = connection
-        if connection == max_connection[0]:
+            max_conn[0] = conn
+        if conn == max_conn[0]:
             min_wire[0] = min(min_wire[0], wire)
         return
 
-    if connection + len(to_connect) < max_connection[0]:
-        return
-
-    if wire > min_wire[0] and connection + len(to_connect) == max_connection[0]:
-        return
+    if conn + len(to_conn) < max_conn[0]: return
+    if wire > min_wire[0] and conn + len(to_conn) == max_conn[0]: return
     
-    peeked = to_connect[-1]
-    for direction in directions:
-        if peek_path(n, cores, peeked, direction):
-            popped = to_connect.pop()
-            wire += connect_path(n, cores, popped, direction, "connect")
-            solution(n, cores, connection+1, max_connection, to_connect, wire, min_wire)
-            wire -= connect_path(n, cores, popped, direction, "disconnect")
-            to_connect.append(popped)
-    popped = to_connect.pop()
-    solution(n, cores, connection, max_connection, to_connect, wire, min_wire)
-    to_connect.append(popped)
+    peeked = to_conn[-1]
+    for d in range(4):
+        if is_connectable(n, cores, peeked, d):
+            popped = to_conn.pop()
+            wire += connect(n, cores, popped, d, 1)
+            dfs(n, cores, to_conn, conn+1, max_conn, wire, min_wire)
+            wire -= connect(n, cores, popped, d, 0)
+            to_conn.append(popped)
+    popped = to_conn.pop()
+    dfs(n, cores, to_conn, conn, max_conn, wire, min_wire)
+    to_conn.append(popped)
 
-def peek_path(n, cores, core, direction):
-    i, j = core
-    connectable = True
-    if direction == "up":
-        while i > 0:
-            i -= 1
-            if cores[i][j] == 1: connectable = False
-    if direction == "down":
-        while i < n-1:
-            i += 1
-            if cores[i][j] == 1: connectable = False
-    if direction == "right":
-        while j < n-1:
-            j += 1
-            if cores[i][j] == 1: connectable = False
-    if direction == "left":
-        while j > 0:
-            j -= 1
-            if cores[i][j] == 1: connectable = False
-    return connectable
+def is_connectable(n, cores, core, d):
+    x, y = core
+    while True:
+        x += DX[d]
+        y += DY[d]
+        if x < 0 or y < 0 or x >= n or y >= n: return True
+        if cores[x][y] == 1: return False
 
-def connect_path(n, cores, core, direction, connection):
-    connection_map = {"connect": 1, "disconnect": 0}
-    i, j = core
+def connect(n, cores, core, d, connect):
+    x, y = core
     wire = 0
-    if direction == "up":
-        while i > 0:
-            i -= 1
-            cores[i][j] = connection_map[connection]
-            wire += 1
-    if direction == "down":
-        while i < n-1:
-            i += 1
-            cores[i][j] = connection_map[connection]
-            wire += 1
-    if direction == "right":
-        while j < n-1:
-            j += 1
-            cores[i][j] = connection_map[connection]
-            wire += 1
-    if direction == "left":
-        while j > 0:
-            j -= 1
-            cores[i][j] = connection_map[connection]
-            wire += 1
-    return wire
-   
-main()
-
-'''
-3  
-7    
-0 0 1 0 0 0 0
-0 0 1 0 0 0 0
-0 0 0 0 0 1 0
-0 0 0 0 0 0 0
-1 1 0 1 0 0 0
-0 1 0 0 0 0 0
-0 0 0 0 0 0 0
-9  
-0 0 0 0 0 0 0 0 0
-0 0 1 0 0 0 0 0 1
-1 0 0 0 0 0 0 0 0
-0 0 0 1 0 0 0 0 0
-0 1 0 0 0 0 0 0 0
-0 0 0 0 0 0 1 0 0
-0 0 0 1 0 0 0 0 0
-0 0 0 0 0 0 0 1 0
-0 0 0 0 0 0 0 0 1
-11 
-0 0 1 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 1
-0 0 0 1 0 0 0 0 1 0 0
-0 1 0 1 1 0 0 0 1 0 0
-0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 1 0 0 0
-0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 1 0 0
-0 0 0 0 0 0 1 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0
-'''
-
-'''
-1
-9
-0 0 0 1 0 1 1 1 0
-1 0 0 1 0 0 0 0 0
-1 0 0 1 0 0 0 0 0
-1 0 0 0 0 0 1 1 1
-0 0 0 0 1 0 0 0 0
-1 1 1 0 0 0 0 0 1
-0 0 0 0 0 1 0 0 1
-0 0 0 0 0 1 0 0 1
-0 1 1 1 0 1 0 0 0
-'''
+    while True:
+        x += DX[d]
+        y += DY[d]
+        if x < 0 or y < 0 or x >= n or y >= n: return wire
+        cores[x][y] = connect
+        wire += 1
+        
+if __name__ == "__main__":
+    main()
