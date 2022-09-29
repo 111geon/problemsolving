f4f55810125de54ef87e8c44efa3fd567630a917
111geon <yangdaz3@gmail.com>
2022-09-29 23:43:52 +0900

    후덜덜한 카카오

---
+++ programmers/2022KAKAOBLINDRECRUITMENT/주차요금계산.py
@@ -0,0 +1,34 @@
+import collections
+import math
+
+def solution(fees, records):
+    cumul_times = collections.defaultdict(int)
+    times = dict()
+    for record in records:
+        time, num, inout = record.split()
+        hour, minute = map(int, time.split(":"))
+        minutes = 60 * hour + minute
+        if inout == "IN": 
+            times[num] = minutes
+        else:
+            cumul_times[num] += minutes - times[num]
+            times[num] = -1
+    
+    for num, time in times.items():
+        if time == -1: continue
+        cumul_times[num] += 23 * 60 + 59 - time
+    
+    answer = []
+    for num in sorted(cumul_times):
+        money = fees[1]
+        time = cumul_times[num]
+        if time <= fees[0]: 
+            answer.append(money)
+            continue
+        time -= fees[0]
+        time /= fees[2]
+        time = math.ceil(time)
+        money += time * fees[3]
+        answer.append(money)
+    
+    return answer
